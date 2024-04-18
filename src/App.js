396f03aa24a7cd0d49af6d2ecd814503bd95f885
Devesh Vishal <deveshvishal15@gmail.com>
2024-04-18 14:18:30 +0530

    React_NotesKeeper Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import CreateArea from './components/CreateArea.js';
+import Note from './components/Note.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const[note,setNote] = useState([]);
+    
+    const addNote = function(newNote) {
+        setNote((prevNotes) => {
+        return [...prevNotes , newNote]
+    })
+    }
+    
+    const onDelete = function(id){
+        setNote((prevNote) => {
+            return prevNote.filter((noteItem,index) => {
+                return index !== id;
+            })
+        })
+    }
+    
+
+    return (
+        <div className="App">
+        <Header></Header>
+        <CreateArea onAdd={addNote}></CreateArea>
+        {note.map((noteItem,index) => {
+        return(
+            <Note key={index} id={index} title={noteItem.title} content={noteItem.content} deleteNote={onDelete}></Note>
+        )
+    })}
+        <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
